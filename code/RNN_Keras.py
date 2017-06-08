64b6120ebf6c174db27fc2f86d9b49c13f263e24
Peiran <644425208@qq.com>
2017-06-08 11:26:26 -0400

    Add files via upload

--- code/RNN_Keras.py
+++ code/RNN_Keras.py
@@ -1,14 +1,8 @@
-'''
-Example of an LSTM model with GloVe embeddings along with magic features
+# The code is modified from https://www.kaggle.com/lystdo/lb-0-18-lstm-with-glove-and-magic-features
+# Great Appreciation
 
-Tested under Keras 2.0 with Tensorflow 1.0 backend
-
-Single model may achieve LB scores at around 0.18+, average ensembles can get 0.17+
-'''
-
-########################################
-## import packages
-########################################
+#########################################
+# import packages
 import os
 import re
 import csv
@@ -33,41 +27,44 @@ from keras.callbacks import EarlyStopping, ModelCheckpoint
 from sklearn.preprocessing import StandardScaler
 
 import sys
-# reload(sys)
-# sys.setdefaultencoding('utf-8')
+#########################################
 
-########################################
-## set directories and parameters
-########################################
-BASE_DIR = '../input/'
-EMBEDDING_FILE = BASE_DIR + 'glove.840B.300d.txt'
-TRAIN_DATA_FILE = BASE_DIR + 'train.csv'
-TEST_DATA_FILE = BASE_DIR + 'test.csv'
+
+
+#########################################
+# define constants and parameters
+INPUT_DIR = '../input/'
+EMBEDDING_FILE = INPUT_DIR + 'glove.840B.300d.txt'
+TRAIN_DATA_FILE = INPUT_DIR + 'train.csv'
+TEST_DATA_FILE = INPUT_DIR + 'test.csv'
 MAX_SEQUENCE_LENGTH = 30
 MAX_NB_WORDS = 200000
 EMBEDDING_DIM = 300
-VALIDATION_SPLIT = 0.1
+VALIDATION_SPLIT_RATIO = 0.1
 
 num_lstm = np.random.randint(175, 275)
-num_dense1 = np.random.randint(100, 150)
-num_dense2 = np.random.randint(100, 150)
+num_dense1 = np.random.randint(200, 250)
+num_dense2 = np.random.randint(150, 200)
 rate_drop_lstm = 0.15 + np.random.rand() * 0.25
 rate_drop_dense = 0.15 + np.random.rand() * 0.25
 
 act = 'relu'
 re_weight = True # whether to re-weight classes to fit the 17.5% share in test set
 
-STAMP = 'lstm_%d_%d_%.2f_%.2f'%(num_lstm, num_dense1, num_dense2, rate_drop_lstm, \
+STAMP = 'lstm_%d_%d %d_%.2f_%.2f'%(num_lstm, num_dense1, num_dense2, rate_drop_lstm, \
         rate_drop_dense)
+#########################################
 
-########################################
-## index word vectors
-########################################
-print('Indexing word vectors')
+
+
+#########################################
+# Create word embedding dictionary from 'glove.840B.300d.txt'
+print('Create word embedding dictionary')
 
 embeddings_index = {}
 f = open(EMBEDDING_FILE)
 count = 0
+
 for line in f:
     values = line.split()
     word = values[0]
@@ -76,17 +73,20 @@ for line in f:
 f.close()
 
 print('Found %d word vectors of glove.' % len(embeddings_index))
+#########################################
 
-########################################
-## process texts in datasets
-########################################
+
+
+#########################################
+# process texts in datasets
 print('Processing text dataset')
 
-# The function "text_to_wordlist" is from
-# https://www.kaggle.com/currie32/quora-question-pairs/the-importance-of-cleaning-text
 def text_to_wordlist(text, remove_stopwords=False, stem_words=False):
+	"""
+	Function from https://www.kaggle.com/currie32/quora-question-pairs/the-importance-of-cleaning-text
+	"""
+
     # Clean the text, with the option to remove stopwords and to stem words.
-    
     # Convert words to lower case and split them
     text = text.lower().split()
 
@@ -138,6 +138,7 @@ def text_to_wordlist(text, remove_stopwords=False, stem_words=False):
     # Return a list of words
     return(text)
 
+# load data and process with text_to_wordlist
 texts_1 = [] 
 texts_2 = []
 labels = []
@@ -162,6 +163,7 @@ with codecs.open(TEST_DATA_FILE, encoding='utf-8') as f:
         test_ids.append(values[0])
 print('Found %s texts in test.csv' % len(test_texts_1))
 
+# Tokenize words in all sentences
 tokenizer = Tokenizer(num_words=MAX_NB_WORDS)
 tokenizer.fit_on_texts(texts_1 + texts_2 + test_texts_1 + test_texts_2)
 
@@ -173,20 +175,23 @@ test_sequences_2 = tokenizer.texts_to_sequences(test_texts_2)
 word_index = tokenizer.word_index
 print('Found %s unique tokens' % len(word_index))
 
+# pad all train with MAX_SEQUENCE_LENGTH
 data_1 = pad_sequences(sequences_1, maxlen=MAX_SEQUENCE_LENGTH)
 data_2 = pad_sequences(sequences_2, maxlen=MAX_SEQUENCE_LENGTH)
 labels = np.array(labels)
 print('Shape of data tensor:', data_1.shape)
 print('Shape of label tensor:', labels.shape)
 
+# pad all test with MAX_SEQUENCE_LENGTH
 test_data_1 = pad_sequences(test_sequences_1, maxlen=MAX_SEQUENCE_LENGTH)
 test_data_2 = pad_sequences(test_sequences_2, maxlen=MAX_SEQUENCE_LENGTH)
 test_ids = np.array(test_ids)
-
-########################################
-## generate leaky features
 ########################################
 
+
+
+########################################
+# leaky features
 train_df = pd.read_csv(TRAIN_DATA_FILE)
 test_df = pd.read_csv(TEST_DATA_FILE)
 
@@ -221,10 +226,12 @@ ss = StandardScaler()
 ss.fit(np.vstack((leaks, test_leaks)))
 leaks = ss.transform(leaks)
 test_leaks = ss.transform(test_leaks)
+#########################################
 
-########################################
-## prepare embeddings
-########################################
+
+
+#########################################
+# Create embedding matrix for embedding layer
 print('Preparing embedding matrix')
 
 nb_words = min(MAX_NB_WORDS, len(word_index))+1
@@ -235,11 +242,13 @@ for word, i in word_index.items():
     if embedding_vector is not None:
         embedding_matrix[i] = embedding_vector
 print('Null word embeddings: %d' % np.sum(np.sum(embedding_matrix, axis=1) == 0))
+#########################################
 
-########################################
-## sample train/validation data
-########################################
-#np.random.seed(1234)
+
+
+#########################################
+# Train Validation split
+np.random.seed(1992)
 perm = np.random.permutation(len(data_1))
 idx_train = perm[:int(len(data_1)*(1-VALIDATION_SPLIT))]
 idx_val = perm[int(len(data_1)*(1-VALIDATION_SPLIT)):]
@@ -258,16 +267,19 @@ weight_val = np.ones(len(labels_val))
 if re_weight:
     weight_val *= 0.472001959
     weight_val[labels_val==0] = 1.309028344
+#########################################
 
-########################################
-## define the model structure
-########################################
+
+
+#########################################
+# define the model graph
 embedding_layer = Embedding(nb_words,
         EMBEDDING_DIM,
         weights=[embedding_matrix],
         input_length=MAX_SEQUENCE_LENGTH,
         trainable=False)
 lstm_layer = LSTM(num_lstm, dropout=rate_drop_lstm, recurrent_dropout=rate_drop_lstm)
+#lstm_layer2 = LSTM(num_lstm, dropout=rate_drop_lstm, recurrent_dropout=rate_drop_lstm)
 
 sequence_1_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
 embedded_sequences_1 = embedding_layer(sequence_1_input)
@@ -275,56 +287,78 @@ x1 = lstm_layer(embedded_sequences_1)
 
 sequence_2_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
 embedded_sequences_2 = embedding_layer(sequence_2_input)
-y1 = lstm_layer(embedded_sequences_2)
+x2 = lstm_layer(embedded_sequences_2)
 
 leaks_input = Input(shape=(leaks.shape[1],))
 leaks_dense = Dense(num_dense1/2, activation=act)(leaks_input)
 
-merged = concatenate([x1, y1, leaks_dense])
+merged = concatenate([x1, x2, leaks_dense])
+
+#########################################
+# Another structure
+#def caldist(cat):
+#    return tf.reduce_sum(tf.square(tf.subtract(cat[0],cat[1])), axis=1, keep_dims=True)
+#def calangle(cat):
+#    return tf.reduce_sum(tf.multiply(cat[0],cat[1]), axis=1, keep_dims=True)
+
+#distance = Lambda(caldist)([x1,y1])
+#angle = Lambda(calangle)([x1,y1])
+
+#merged = concatenate([x1, y1, distance, angle, leaks_dense])
+#########################################
+
+merged = BatchNormalization()(merged)
 merged = Dense(num_dense1, activation=act)(merged)
+
 merged = BatchNormalization()(merged)
 merged = Dropout(rate_drop_dense)(merged)
-
 merged = Dense(num_dense2, activation=act)(merged)
+
 merged = BatchNormalization()(merged)
 merged = Dropout(rate_drop_dense)(merged)
 
 preds = Dense(1, activation='sigmoid')(merged)
+#########################################
 
-########################################
-## add class weight
-########################################
+
+
+#########################################
+# add class weight
 if re_weight:
     class_weight = {0: 1.309028344, 1: 0.472001959}
 else:
     class_weight = None
+#########################################
 
-########################################
-## train the model
-########################################
+
+
+#########################################
+# train the model
 model = Model(inputs=[sequence_1_input, sequence_2_input, leaks_input], \
         outputs=preds)
 model.compile(loss='binary_crossentropy',
         optimizer='adadelta',
         metrics=['acc'])
-#model.summary()
 print(STAMP)
 
-early_stopping =EarlyStopping(monitor='val_loss', patience=5)
-bst_model_path = STAMP + '.h5'
+# set early stopping (large patience should be usefule)
+early_stopping =EarlyStopping(monitor='val_loss', patience=20)
+bst_model_path = STAMP + '.h5' 
 model_checkpoint = ModelCheckpoint(bst_model_path, save_best_only=True, save_weights_only=True)
 
 hist = model.fit([data_1_train, data_2_train, leaks_train], labels_train, \
         validation_data=([data_1_val, data_2_val, leaks_val], labels_val, weight_val), \
-        epochs=300, batch_size=2048, shuffle=True, \
+        epochs=200, batch_size=2048, shuffle=True, \
         class_weight=class_weight, callbacks=[early_stopping, model_checkpoint])
 
-model.load_weights(bst_model_path)
+model.load_weights(bst_model_path) # sotre model parameters in .h5 file
 bst_val_score = min(hist.history['val_loss'])
+#########################################
+
+
 
 ########################################
-## make the submission
-########################################
+# make the submission
 print('Start making the submission before fine-tuning')
 
 preds = model.predict([test_data_1, test_data_2, test_leaks], batch_size=8192, verbose=1)
@@ -333,3 +367,4 @@ preds /= 2
 
 submission = pd.DataFrame({'test_id':test_ids, 'is_duplicate':preds.ravel()})
 submission.to_csv('%.4f_'%(bst_val_score)+STAMP+'.csv', index=False)
+#########################################
